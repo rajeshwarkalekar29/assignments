a90f2dbab6102d4d1857fc68e8f79118da6a69da
rajeshwarkalekar29 <111868184+rajeshwarkalekar29@users.noreply.github.com>
2022-10-08 00:37:44 +0530

    Add files via upload

---
+++ practice/Rough.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+      int add(int a,int b);
+     printf("enter two numbers");
+     scanf("%d %d",&a,&b);
+     sum(a,b);
+     printf("a=%d,b=%d,sum=%d",a,b,add);
+	
+	return 0;
+}
