a34a906636cea2355d658dcbdbe2458e4482483d
rajeshwarkalekar29 <111868184+rajeshwarkalekar29@users.noreply.github.com>
2022-10-20 18:47:39 +0530

    Add files via upload

---
+++ Assignment3/Compare_Given_numbers.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main ()
+{
+	int num1, num2 ;
+	puts("enter 2 integers");
+	scanf("%d %d",&num1, &num2);
+	printf("\nnum1 = %d\nnum2 = %d\n",num1, num2);
+	
+	if(num1 != num2)
+	{
+		if(num1 > num2)
+		{
+			printf("%d > %d",num1, num2);
+		}
+		else
+		{ 
+			printf("%d < %d",num1, num2);
+		}
+	}
+	else 
+	{
+		printf("%d = %d",num1, num2);
+	}
+	return 0;
+}
